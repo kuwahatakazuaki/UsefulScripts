fff2f6d5b7e8718b9ee2cd4577632068ab39959c
kuwahatakazuaki <kuwahata0615@gmail.com>
2022-10-09 09:19:44 +0900

    change vasp_md_proper.sh

--- VASP_md_proper.sh
+++ VASP_md_proper.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-grep " T=" OSZICAR |awk '{print $1, $3, $5}' > temperature.out
-grep "volume" vasprun.xml |awk '{print $3}' > volume.out
+out1='temperature.out'
+out2='volume.out'
+out3='md.out'
+
+grep " T=" OSZICAR |awk '{print $1, $3, $5}' > $out1
+grep "volume" vasprun.xml |sed '1,3d' |awk '{print $3}' > $out2
+
+paste -d " " $out1 $out2 > $out3
+
+rm $out1 $out2
+
 
